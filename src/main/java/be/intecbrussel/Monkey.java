300548ace3916de17278771e4608dda854dde219
Elmaz <Kakbyelmaz@gmail.com>
2024-01-18 15:24:25 +0100

    Initial commit

--- src/main/java/be/intecbrussel/Monkey.java
+++ src/main/java/be/intecbrussel/Monkey.java
@@ -1,2 +1,27 @@
-package be.intecbrussel;public class Monkey {
+package be.intecbrussel;
+
+public class Monkey extends Animal {
+    private boolean isHyperActive;
+
+    public Monkey() {
+    }
+
+    public Monkey(boolean isClean, int age, String name, int animalNumber, boolean isHyperActive) {
+        super(isClean, age, name, animalNumber);
+        this.isHyperActive = isHyperActive;
+    }
+
+    public boolean isHyperActive() {
+        return isHyperActive;
+    }
+
+    public void setHyperActive(boolean hyperActive) {
+        isHyperActive = hyperActive;
+    }
+
+    @Override
+    public void treatAnimal() {
+        super.setClean(true);
+        this.setHyperActive(false);
+    }
 }
